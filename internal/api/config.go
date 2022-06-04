41dc7e23e062b81ab97a0fc9602c2df1ae4e0526
Jiajie Chen <kewellcjj@gmail.com>
2022-06-04 23:22:22 +0000

    fix:use env port

--- internal/api/config.go
+++ internal/api/config.go
@@ -3,7 +3,7 @@ package api
 import "time"
 
 type Config struct {
-	Port            string        `yaml:"port" env:"SERVER_PORT" env-description:"Server port" env-default:"1927"`
+	Port            string        `env:"PORT" env-description:"Server port" env-default:"1927"`
 	NewRelicAPIKey  string        `env:"NEWRELIC_API_KEY" env-description:"API key for NewRelic"`
 	NewRelicAppName string        `yaml:"newrelic_app_name" env:"NEWRELIC_APP_NAME" env-description:"App name for NewRelic"`
 	DATABASE_URL    string        `env:"DATABASE_URL" env-description:"Database URL" env-default:"host=localhost user=postgres password=postgres dbname=postgres port=5432"`
